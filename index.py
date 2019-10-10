204d492c836a378bf3dbdf246a6bb882fc6c7fde
Valeriia Terova <terovaleriya@gmail.com>
2019-10-10 05:06:56 +0300

    Чтение массива со страницами

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
-import itertools
 import re
 import string
 from collections import defaultdict
-from itertools import islice
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 
-s = {"и", "а", "в", "я", "на", "не", "что", "с", "она", "они", "оно", "он", "как", "мне", "меня",
-     "но", "его", "ее", "это", "к", "так", "за", "по", "же"}
+skip_words = {"и", "а", "в", "я", "на", "не", "что",
+              "с", "она", "они", "оно", "он", "как",
+              "мне", "меня", "но", "его", "ее", "это",
+              "к", "так", "за", "по", "же", "из", "то",
+              "о", "от"}
 
 
 class Entry:
@@ -17,7 +18,7 @@ class Entry:
         self.pages = defaultdict(lambda: 0)
         self.count = count
         if pages is not None:
-            for p, c in pages.items():
+            for p, c in pages:
                 self.pages[p] = c
 
     def add_page(self, page: int):
@@ -25,14 +26,21 @@ class Entry:
         self.count = self.count + 1
 
     def __repr__(self) -> str:
-        return str(self.count)
+        def format_pages(x: Tuple[int, int]) -> str:
+            page, count = x
+            return str(page) + ('(' + str(count) + ')' if count > 1 else '')
+
+        pages = ', '.join(map(format_pages, self.pages.items()))
+        return str(self.count) + ', ' + pages
 
 
 class Index:
+    number_of_lines_per_page: int
     entries: Dict[str, Entry]
 
     def __init__(self):
         self.entries = defaultdict(lambda: Entry())
+        self.number_of_lines_per_page = 45
 
     def add_word(self, word: str, page: int):
         entry = self.entries[word]
@@ -42,8 +50,8 @@ class Index:
         with open(filename, 'rt') as fp:
             for cnt, line in enumerate(fp):
                 words = re.sub('[' + string.punctuation + string.ascii_letters + 'à-ö0-9]', '', line.lower()).split()
-                words = set(words) - s
-                page = cnt // 45 + 1
+                words = set(words) - skip_words
+                page = cnt // self.number_of_lines_per_page + 1
                 for word in words:
                     self.add_word(word, page)
 
@@ -60,12 +68,21 @@ class Index:
                     fp.write(", ")
                 fp.write("\n")
 
+    @staticmethod
+    def read_pages(t: Tuple[str, str]):
+        if t[1]:
+            return int(t[0]), int(t[1])
+        else:
+            return int(t[0]), 1
+
     def load(self, filename: str):
         with open(filename, 'rt') as fp:
             for line in fp:
-                word, count, pages = line.split(":")
-                self.entries[word] = Entry(int(count))  # pages
+                word, count, pstr = line.split(":")
+                pages = list(map(self.read_pages, re.findall(r'(\d+)(?:\((\d+)\))?, ', pstr)))
+                entry = Entry(int(count), pages)
+                self.entries[word] = entry  # pages
 
-    def most_freq_used(self, count: object) -> Tuple[str, Entry]:
+    def most_freq_used(self, count: int) -> Tuple[str, Entry]:
         swords: Tuple[str, Entry] = sorted(self.entries.items(), key=lambda kv: -kv[1].count)
-        return swords[0: count]
+        return swords[:count]
